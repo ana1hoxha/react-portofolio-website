7fcb530176dde3996468ea5901d3f55da64a96c4
ana1hoxha <ana1hoxha@gmail.com>
2022-10-14 10:46:36 +0200

    basis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import Portofolio from './components/portofolio/Portofolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Services from './components/services/Services'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+  {/* keto jane seksionet qe dua te perfshi ne website */}
+    <Header/>
+    <Nav/>
+    <About/>
+    <Experience/>
+    <Services/>
+    <Portofolio/>
+    <Testimonials/>
+    <Contact/>
+    <Footer/>
+    </>
+  )
 }
 
-export default App;
+export default App
